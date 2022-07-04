ab4cf628736918eeedfa7831199e16d3894785f8
Jon Soloway <jon.soloway@hmcts.net>
2022-07-04 17:06:21 +0100

    Updating script to Grant Issued stage

--- src/gatling/simulations/scenarios/ProbateApp_CWIssueGrant.scala
+++ src/gatling/simulations/scenarios/ProbateApp_CWIssueGrant.scala
@@ -35,8 +35,45 @@ object ProbateApp_CWIssueGrant {
       .check(jsonPath("$.documents[0]._links.self.href").saveAs("DocumentURL"))
       .check(jsonPath("$.documents[0].hashToken").saveAs("hashToken")))
 
+    .pause(1)
+
     //Link uploaded document to case
-    .exec(CCDAPI.CreateEvent("Caseworker", "PROBATE", "GrantOfRepresentation", "boUploadDocumentsForCaseCreated", "bodies/CCD_Probate_DocumentUpload.json"))
+    .exec(CCDAPI.CreateEvent("Caseworker", "PROBATE", "GrantOfRepresentation", "boUploadDocumentsForCaseCreated", "bodies/UploadDocumentsForCaseCreated.json"))
+
+    .pause(1)
+
+    //Print the case
+    .exec(CCDAPI.CreateEvent("Caseworker", "PROBATE", "GrantOfRepresentation", "boPrintCase", "bodies/CCD_PrintCase.json"))
+
+    .pause(1)
+
+    //Mark as ready for examination
+    .exec(CCDAPI.CreateEvent("Caseworker", "PROBATE", "GrantOfRepresentation", "boMarkAsReadyForExamination", "bodies/CCD_MarkAsReadyForExamination.json"))
+
+    .pause(1)
+
+    //Find matches (Examining)
+    .exec(CCDAPI.CreateEvent("Caseworker", "PROBATE", "GrantOfRepresentation", "boFindMatchesForReadyForExamining", "bodies/CCD_FindMatchesForReadyForExamining.json"))
+
+    .pause(1)
+
+    //Examine case
+    .exec(CCDAPI.CreateEvent("Caseworker", "PROBATE", "GrantOfRepresentation", "boExamineCase", "bodies/CCD_ExamineCase.json"))
+
+    .pause(1)
+
+    //Mark as ready to issue
+    .exec(CCDAPI.CreateEvent("Caseworker", "PROBATE", "GrantOfRepresentation", "boMarkAsReadyToIssue", "bodies/CCD_MarkAsReadyToIssue.json"))
+
+    .pause(1)
+
+    //Find matches (Issue grant)
+    .exec(CCDAPI.CreateEvent("Caseworker", "PROBATE", "GrantOfRepresentation", "boFindMatchesNoQA", "bodies/CCD_FindMatchesNoQA.json"))
+
+    .pause(1)
+
+    //Issue grant
+    .exec(CCDAPI.CreateEvent("Caseworker", "PROBATE", "GrantOfRepresentation", "boIssueGrantForCaseMatching", "bodies/CCD_IssueGrantForCaseMatching.json"))
 
     .pause(1)
   }
